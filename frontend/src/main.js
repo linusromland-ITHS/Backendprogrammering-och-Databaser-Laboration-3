2f53322d807f1e7c0b4f8ef7fd09ec58b4ccf91a
Linus Romland <romlandlinus@gmail.com>
2022-05-09 09:12:58 +0200

    Vue3 init

---
+++ frontend/src/main.js
@@ -0,0 +1,23 @@
+//External Dependencies import:
+import { createApp } from 'vue';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+
+//Internal Dependencies import:
+import App from './App.vue';
+import './styles/tailwind.css';
+
+//Creats the Vue app
+const app = createApp(App);
+
+//Registers vue-toastification
+app.use(Toast, {
+	transition: 'Vue-Toastification__bounce',
+	maxToasts: 20,
+	newestOnTop: true,
+	timeout: 2500,
+	hideProgressBar: true,
+});
+
+//Mounts app to div with id app
+app.mount('#app');
